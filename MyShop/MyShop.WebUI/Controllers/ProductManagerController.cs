b669147a6227b60200457110362d3610dfbe0eec
Adam Ochotnicki <adamochotnicki@gmail.com>
2020-05-13 17:14:38 +0100

    Product grouping added - by categories

--- MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using MyShop.DataAccess.InMemory;
 
 namespace MyShop.WebUI.Controllers
@@ -11,10 +12,12 @@ namespace MyShop.WebUI.Controllers
     public class ProductManagerController : Controller
     {
         ProductRepository context;
+        ProductCategoryRepository productCategories;
 
         public ProductManagerController()
         {
             context = new ProductRepository();
+            productCategories = new ProductCategoryRepository();
         }
 
         // GET: ProductManager
@@ -26,8 +29,12 @@ namespace MyShop.WebUI.Controllers
 
         public ActionResult Create()
         {
-            Product product = new Product();
-            return View(product);
+            ProductManagerViewModel viewModel = new ProductManagerViewModel
+            {
+                Product = new Product(),
+                ProductCategories = productCategories.Collection()
+            };
+            return View(viewModel);
         }
 
         [HttpPost]
@@ -41,7 +48,6 @@ namespace MyShop.WebUI.Controllers
             {
                 context.Insert(product);
                 context.Commit();
-
                 return RedirectToAction("Index");
             }
         }
@@ -55,7 +61,12 @@ namespace MyShop.WebUI.Controllers
             }
             else
             {
-                return View(product);
+                ProductManagerViewModel viewModel = new ProductManagerViewModel
+                {
+                    Product = product,
+                    ProductCategories = productCategories.Collection()
+                };
+                return View(viewModel);
             }
         }
 
